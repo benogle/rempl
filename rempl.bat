f6ebb54df9a87d1f88a88e6753e7a56e903e422b
Ben Ogle <ogle.ben@gmail.com>
2009-04-10 00:00:16 -0700

    cleanup. server arg. better error handling.

--- rempl.bat
+++ rempl.bat
@@ -1,1 +1,7 @@
-python D:\work\git\rempl\rempl.py %1
+rem This is mostly intended to be used as a helper app in firefox for pls and m3u playlist files.
+rem Yes, the path must be absolute as the cwd when called is firefox's app dir. So you have to change it. :(
+
+set AJAXAMP_ADDR=http://192.168.1.100:5151
+set REMPL_PATH=D:\work\git\rempl\rempl.py
+
+python %REMPL_PATH% -s %AJAXAMP_ADDR% %1
