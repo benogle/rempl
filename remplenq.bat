52b40c725aaff53a6ddcd942d0d13c5ff5f98757
Ben Ogle <ogle.ben@gmail.com>
2009-04-17 18:35:14 -0700

    readme + enqueue batch file

---
+++ remplenq.bat
@@ -0,0 +1,7 @@
+rem This is mostly intended to be used as a helper app in firefox for pls and m3u playlist files.
+rem Yes, the path must be absolute as the cwd when called is firefox's app dir. So you have to change it. :(
+
+set AJAXAMP_ADDR=http://192.168.1.100:5151
+set REMPL_PATH=D:\work\git\rempl\rempl.py
+
+python %REMPL_PATH% -e -s %AJAXAMP_ADDR% %1
